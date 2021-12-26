73efec3e5774a8ca3bb5a1f2c18c189c9c1131ea
Patrik Madison <patrik.madison@gmail.com>
2021-12-26 14:42:42 +0100

    add vuex chartjs

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as type from './types';
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
       count: 0
-    }
+    },
+    mutations: {
+      increment (state, payload){
+        console.log('mutat');
+        return state.count = state.count + payload.amount;
+      }
+    },
+    actions: {
+      increment (context, payload) {
+        console.log('action');
+        context.commit(type.Increment, payload);
+      },
+    },
   });
   
 export default store;
--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -4,14 +4,33 @@
       <ul>
         <li> Temp </li>
         <li> Press </li>
-        <li> Humid </li>
+        <li> Humids </li>
       </ul>      
+      <v-btn @click="incr"> increment {{count}} </v-btn>
+      <v-btn @click="decr"> decrement </v-btn>
   </v-container>
 </template>
 
 <script>
+  import store from '../store/store';
+  import * as type from '../store/types';
+  import { mapState } from 'vuex';
+
   export default {
     name: 'Weather',
     data: () => ({ }),
+    methods: {
+      incr: function(){
+        console.log('incr'); 
+        store.dispatch({ type: type.Increment, amount: 20}); 
+      },
+      decr: function(){
+        console.log('decr');
+        store.dispatch({ type: type.Decrement, amount: 20}); 
+      } 
+    },
+    computed: mapState({
+      count: state => state.count,
+    }),      
   }
 </script>
